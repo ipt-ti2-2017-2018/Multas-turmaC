f7b962ab3b6c5d48b5acdf716275cb8a1386e241
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2018-04-20 18:56:54 +0100

    Alteração da ROTA definida por default

--- Multas-tC/Multas-tC/App_Start/RouteConfig.cs
+++ Multas-tC/Multas-tC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Multas_tC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Agentes", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
